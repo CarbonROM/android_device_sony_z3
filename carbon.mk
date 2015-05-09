06157bc71678d1bd50a5ee6b4c2e7db007904035
Myself5 <myself5@carbonrom.org>
2015-05-09 10:45:39 +0200

    z3: carbonify

--- carbon.mk
+++ carbon.mk
@@ -1,8 +1,8 @@
 # Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/carbon/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/carbon/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/sony/z3/full_z3.mk)
 
@@ -10,5 +10,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D6603
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/D6603/D6603:5.0.2/23.1.A.0.690/814068233:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="D6603-user 5.0.2 23.1.A.0.690 814068233 release-keys"
 
-PRODUCT_NAME := cm_z3
+PRODUCT_NAME := carbon_z3
 PRODUCT_DEVICE := z3
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,2 @@
-add_lunch_combo cm_z3-eng
+add_lunch_combo carbon_z3-eng
+add_lunch_combo carbon_z3-userdebug
