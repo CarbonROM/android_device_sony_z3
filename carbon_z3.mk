88d295f0f89f1566428f81cdbc06798e9c71f5e0
Christian Oder <myself5@carbonrom.org>
2017-07-08 14:25:43 +0200

    Z3: Carbonify
    Change-Id: Ic97173d0c161d952cf5f7bc495680d05e81434d6

--- carbon_z3.mk
+++ carbon_z3.mk
@@ -1,5 +1,8 @@
-# Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit Carbon GSM telephony parts
+$(call inherit-product, vendor/carbon/config/gsm.mk)
+
+# Inherit Carbon product configuration
+$(call inherit-product, vendor/carbon/config/common.mk)
 
 $(call inherit-product, device/sony/z3/full_z3.mk)
 
@@ -7,5 +10,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D6603
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/leo/leo:N/NPD56N/1000106:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="leo-user N NPD56N 1000106 release-keys"
 
-PRODUCT_NAME := lineage_z3
+PRODUCT_NAME := carbon_z3
 PRODUCT_DEVICE := z3
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.carbon.maintainer="Myself5"
+
