a1af3fbacfa4b5b465ddf1f23d97583f89ac6e91
Christian Oder <myself5@carbonrom.org>
2018-05-07 20:38:37 +0200

    z3: define BUILD_FINGERPRINT for vendor and system to prevent missmatch
    Change-Id: I5f2cea69953ab59ea7fd6996eec786a09cac1ffa

--- carbon_z3.mk
+++ carbon_z3.mk
@@ -7,8 +7,9 @@ $(call inherit-product, vendor/carbon/config/common.mk)
 $(call inherit-product, device/sony/z3/full_z3.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D6603
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/leo/leo:N/NPD56N/1000106:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="leo-user N NPD56N 1000106 release-keys"
+
+BUILD_FINGERPRINT := Sony/leo/leo:N/NPD56N/1000106:user/release-keys
 
 PRODUCT_NAME := carbon_z3
 PRODUCT_DEVICE := z3
